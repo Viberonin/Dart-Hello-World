22fb85c4fc41169c7cad1482fa731b551ce18876
Vaunzz <evanyazid51@gmail.com>
2023-02-28 09:57:37 +0700

    Hello World 2

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: Center(
           child: Text(
-            'Bonjour!',
+            'Hello, World!',
             style: TextStyle(
               fontSize: 30
             ),
